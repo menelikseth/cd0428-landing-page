657f4b1fad19c9ed6d3a2eec76c96de66a0ece13
menelikseth <135473939+menelikseth@users.noreply.github.com>
2023-10-08 22:15:53 -0400

    fix: correct mistakes in code

--- js/app.js
+++ js/app.js
@@ -26,7 +26,8 @@ const sections = document.getElementsByClassName('landing__container'); //array 
 const navUl = document.getElementById('navbar__list'); //navigation menu
 
 //array of section content
-// TODO: Move content to external text file or database
+//TODO: move section content to textfile or database
+const sectionContent = [
     {
         sectionContentHeading: `Section`,
         sectionContentBody: `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
